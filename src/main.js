31c346aef8773f8044fb23392ba8934c7c3bbf2a
Jess <jhzogari@ucsc.edu>
2023-05-10 21:14:32 -0700

    new game

---
+++ src/main.js
@@ -0,0 +1,22 @@
+/*
+ *  Jessica Zogaric
+ *  Fish Fit
+ *  Approximate time to complete: -
+ *  
+ *  
+ */
+
+//configured the screen so its 640 x 480 and pass the config thru the let game
+let config = {
+    type: Phaser.AUTO,
+    width: 640,
+    height: 480,
+    scene: [ Menu, Play ] //puts the diff scenes we have in the scene array
+  }
+//reservekeyboard vars (no longer needed)
+let keyF, keyR, keyUP, keyDOWN;
+let game = new Phaser.Game(config);
+
+// set UI sizes
+let borderUISize = game.config.height / 15;
+let borderPadding = borderUISize / 3;
